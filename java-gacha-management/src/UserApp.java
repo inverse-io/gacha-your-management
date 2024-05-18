3b5e9841dddb2e7cfabe742de32601cb28baf474
Ada Huang <156732844+inverse-io@users.noreply.github.com>
2024-05-18 18:24:11 -0400

    Update UserApp.java

--- java-gacha-management/src/UserApp.java
+++ java-gacha-management/src/UserApp.java
@@ -5,7 +5,7 @@ import java.util.TimerTask;
 // contains the main method
 public class UserApp {
 	private static Scanner myScan = new Scanner(System.in);
-
+	// public static int pulls = 0;
 
 	public static void main(String[] args) {
 		// creating timer and task to run every X milliseconds
@@ -13,5 +13,12 @@ public class UserApp {
 		TimerTask task = new GachaTimer();
 
 		timer.schedule(task, 0, 2000);
+		// alternate way to do this without entirely new class
+		// timer.schedule(new TimerTask() {
+		// 	@Override
+		// 	public void run() {
+		// 		System.out.println("User obtained a wish and now has a total of " + (++pulls) + " wishes.");
+		// 	}
+		// }, 0, 2000);
 	}
 }
